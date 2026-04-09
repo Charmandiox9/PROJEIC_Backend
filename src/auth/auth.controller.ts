2b392c575c5a538a3c44f1674f3a20cdbeedb504
Daniel Durán <daninsoi924@gmail.com>
2026-04-09 01:46:30 -0400

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,7 +19,19 @@ export class AuthController {
   @UseGuards(AuthGuard('google'))
   async googleAuthRedirect(@Req() req: Request, @Res() res: Response) {
     const jwt = await this.authService.login(req.user);
-    const frontendUrl = this.configService.get<string>('FRONTEND_URL') ?? 'http://localhost:3000';
-    res.redirect(`${frontendUrl}/projeic/auth/success?token=${jwt.accessToken}`);
+
+    // Intenta obtener la variable del panel de Railway
+    let frontendUrl = this.configService.get<string>('FRONTEND_URL');
+
+    // Si no existe, pero detectamos que estamos en producción (RAILWAY_PUBLIC_DOMAIN es inyectada por Railway)
+    if (!frontendUrl && process.env.RAILWAY_ENVIRONMENT) {
+      frontendUrl = 'https://projeicfrontend-production.up.railway.app';
+    } else if (!frontendUrl) {
+      frontendUrl = 'http://localhost:3000';
+    }
+
+    res.redirect(
+      `${frontendUrl}/projeic/auth/success?token=${jwt.accessToken}`,
+    );
   }
 }
